08f9b7c7400b9d41d289294469732ecb6687c998
rishabh-malhotra <rishabhmalhotra110796@gmail.com>
2018-11-15 00:32:15 +0530

    basic skeleton made from scratch

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController:Controller
+    {
+        public IActionResult Index()
+        {
+            throw new InvalidOperationException("Bad Things Happened");
+
+            return View();
+        }
+    }
+}
---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+
+    var x = 0;
+    var s = "";
+
+    console.log("hello");
+
+    var theForm = $("#thorm");
+    console.log(theForm);
+    theForm.hide();
+
+    var button = $("#buyButton");
+
+    button.on("click", function () {
+        console.log("Buying Item");
+    })
+
+    var productInfo = $(".product-props li")
+    productInfo.on("click", function () {
+        console.log("You clicked on:" + $(this).text());
+    });
+
+    var $loginToggle=$("#login-toggle")
+    var $popupForm = $("#popup-form")
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    });
+
+});
+
+
